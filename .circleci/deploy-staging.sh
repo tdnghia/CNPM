30882369e0f3ec2f0a93206f6c1a4ebbbda177f9
Ngo Truong Quoc <ngotruongquoc0102@gmail.com>
2020-09-27 15:50:55 +0700

    feat(): change ssh configure

--- .circleci/deploy-staging.sh
+++ .circleci/deploy-staging.sh
@@ -6,7 +6,7 @@ ssh root@3.15.214.83 "cd /home/CNPM && rimraf dist"
 
 rsync -avzP . root@3.15.214.83:/home/CNPM
 
-ssh root@3.15.214.83 "cd /home/CNPM && npm install && pm2 update && exit"
+# ssh root@3.15.214.83 "cd /home/CNPM && npm install && pm2 update && exit"
 
 
 ssh root@3.15.214.83 "cd /home/CNPM && git pull origin master && npm install && npm run build && pm2 update && exit"
