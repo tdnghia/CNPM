cc4d534297176f85df5fe53170f9cc70e459654c
Ngo Truong Quoc <ngotruongquoc0102@gmail.com>
2020-09-29 11:00:58 +0700

    update

--- .circleci/deploy-staging.sh
+++ .circleci/deploy-staging.sh
@@ -2,10 +2,10 @@
 rm -rf .gitignore .eslintignore .eslintrc .circleci node_modules src test
 rm .env.example README.md
 
-ssh root@3.15.214.83 "cd /home/CNPM && rimraf dist"
+ssh root@3.15.223.186 "cd /home/CNPM && rimraf dist"
 
-rsync -avzP . root@3.15.214.83:/home/CNPM
+rsync -avzP . root@3.15.223.186:/home/CNPM
 
-ssh root@3.15.214.83 "cd /home/CNPM && npm install && pm2 update && exit"
+ssh root@3.15.223.186 "cd /home/CNPM && npm install && pm2 update && exit"
 
 # ssh root@3.15.214.83 "cd /home/CNPM && git pull origin master && npm install && npm run build && pm2 update && exit"
