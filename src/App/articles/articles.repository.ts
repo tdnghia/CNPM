f367e0fa87c7f112533fe54e9ecc6fa4e76dfc24
truongquoc <ngotruongquoc0102@gmail.com>
2020-11-06 15:46:50 +0700

    update

--- src/App/articles/articles.repository.ts
+++ src/App/articles/articles.repository.ts
@@ -1,5 +1,6 @@
-import { Article } from "src/entity/article.entity";
-import { EntityRepository, Repository } from "typeorm";
+import { BaseRepository } from 'src/common/Base/base.repository';
+import { Article } from 'src/entity/article.entity';
+import { EntityRepository, Repository } from 'typeorm';
 
 @EntityRepository(Article)
-export class ArticleRepository extends Repository<Article> {}
+export class ArticleRepository extends BaseRepository<Article> {}
