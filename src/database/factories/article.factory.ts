a5007655c6540b4b8a73dde88cb7d33f53fa6eb4
truongquoc <ngotruongquoc0102@gmail.com>
2020-12-22 15:02:39 +0700

    Update seed

--- src/database/factories/article.factory.ts
+++ src/database/factories/article.factory.ts
@@ -1,26 +1,19 @@
-import { Article } from "../../entity/article.entity";
-import { define } from "typeorm-seeding";
+import { Article } from '../../entity/article.entity';
+import { define } from 'typeorm-seeding';
 import * as Faker from 'faker';
 import { getSlug } from '../../core/utils/helper';
 
-define(Article, (
-    faker: typeof Faker,
-    context: { payload?: Article },
-) => {
-    const { payload } = context;
-    const title = payload.title || faker.lorem.sentence(5);
-    const slug = getSlug(title);
+define(Article, (faker: typeof Faker, context: { payload?: Article }) => {
+  const { payload } = context;
+  const title = payload.title || faker.lorem.sentence(5);
+  const slug = getSlug(title);
 
-    const article = new Article();
-    console.log('userId', payload.userId);
-    console.log('categoryId', payload.categoryId);
-    // article.userId = payload.userId;
-    // article.categoryId = payload.categoryId;
-    article.title = title;
-    article.slug = slug;
-    article.user = payload.user;
-    article.content = faker.lorem.paragraph();
-    article.salary_range = payload.salary_range;
+  const article = new Article();
+  article.title = title;
+  article.slug = slug;
+  article.user = payload.user;
+  article.content = faker.lorem.paragraph();
+  // article.salary_range = payload.salaryRange;salaryRange
 
-    return article;
+  return article;
 });
--- src/database/seeds/10-article.seed.ts
+++ src/database/seeds/10-article.seed.ts
@@ -5,22 +5,22 @@ import { Category } from '../../entity/category.entity';
 import { Article } from '../../entity/article.entity';
 
 export default class CreateArticles implements Seeder {
-    public async run(factory: Factory, connection: Connection): Promise<any> {
-        const userRepository = connection.getRepository(User);
-        const categoryRepository = connection.getRepository(Category);
-        const articleRepository = connection.getRepository(Article);
+  public async run(factory: Factory, connection: Connection): Promise<any> {
+    const userRepository = connection.getRepository(User);
+    const categoryRepository = connection.getRepository(Category);
+    const articleRepository = connection.getRepository(Article);
 
-        const user = await userRepository.find({ roleId: 3})
-        const category = await categoryRepository.find();
-        console.log(category);
-        for (let i = 0; i < 50; i++) {
-            await factory(Article)({
-                payload: {
-                    user: user[Math.floor(Math.random() * user.length)].id,
-                    category: category[Math.floor(Math.random() * category.length)].id,
-                    salary_range: '1000 - 2000',
-                }
-              }).create();
-        }
+    const user = await userRepository.find({ roleId: 3 });
+    const category = await categoryRepository.find();
+    console.log(category);
+    for (let i = 0; i < 50; i++) {
+      await factory(Article)({
+        payload: {
+          user: user[Math.floor(Math.random() * user.length)].id,
+          category: category[Math.floor(Math.random() * category.length)].id,
+          salaryRange: '1000 - 2000',
+        },
+      }).create();
     }
+  }
 }
