7b2b2b7335684ee13dd6454434fe6aa2bfe65e9f
truongquoc <ngotruongquoc0102@gmail.com>
2020-12-25 18:48:39 +0700

    update get One

--- src/App/jobs/jobs.controller.ts
+++ src/App/jobs/jobs.controller.ts
@@ -311,13 +311,8 @@ export class JobsController extends BaseController<Job> {
   }
 
   @Override('getOneBase')
-  @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
-  async getOne(
-    @ParsedRequest() req: CrudRequest,
-    @ParsedBody() dto: Job,
-    @UserSession() user,
-  ) {
+  async getOne(@ParsedRequest() req: CrudRequest) {
     try {
       const data = await this.base.getOneBase(req);
       return data;
@@ -352,8 +347,6 @@ export class JobsController extends BaseController<Job> {
       }
       return { ...job, applied: false };
     } catch (error) {
-      console.log('err', error);
-
       throw new HttpException(
         {
           message: 'Job not found',
